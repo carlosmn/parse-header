e753a85d5c83a735731f05ff10c0406064e4066b
Carlos Martín Nieto <carlos@cmartin.tk>
2012-05-22 18:02:43 +0200

    Allow pretty much anything as part of the enum's value

--- parser.rb
+++ parser.rb
@@ -14,8 +14,7 @@ class Parser < Parslet::Parser
   rule(:alpha) { match('[a-zA-Z_]') }
   rule(:xdigit) { digit | match('[a-fA-F]') }
 
-  rule(:equals) { spaces? >> str('=') >> spaces?}
-#  rule(:comma) { spaces? >> str(',') >> spaces? }
+  rule(:assign) { spaces? >> str('=') >> spaces?}
   rule(:left_brace) { spaces? >> str('{') >> spaces? }
   rule(:right_brace) { spaces? >> str('}') >> spaces? }
 
@@ -33,41 +32,6 @@ class Parser < Parslet::Parser
   :right_paren => ')',
   :member_access => '.',
   :question_mark => '?'
-
-  rule(:constant_expression) { conditional_expression }
-  rule(:constant_expression?) { constant_expression.maybe }
-
-  rule(:logical_and_expression) {
-    (
-     inclusive_or_expression.as(:left) >>
-     logical_and >>
-     logical_and_expression.as(:right)
-     ).as(:logical_and) | inclusive_or_expression
-  }
-
-  rule(:logical_or_expression) {
-    (
-     logical_and_expression.as(:left) >>
-     logical_or >>
-     logical_or_expression.as(:right)
-     ).as(:logical_or) | logical_and_expression
-  }
-
-  rule(:conditional_expression) {
-    (
-     logical_or_expression.as(:condition) >> question_mark >>
-     expression.as(:true) >> colon >>
-     conditional_expression.as(:false)
-     ).as(:conditional) | logical_or_expression
-  }
-
-  rule(:assignment_expression) {
-    (
-     unary_expression.as(:left) >>
-     assignment_operator >>
-     assignment_expression.as(:right)
-     ).as(:assign) | conditional_expression
-  }
 
   rule(:identifier) {
     (alpha >> (alpha | digit).repeat).as(:identifier) >> spaces?
@@ -88,7 +52,7 @@ class Parser < Parslet::Parser
   }
 
   rule(:enum_entry) {
-    identifier >> (equals >> (digit).as(:value)).maybe
+    identifier >> (assign >> ((match('[,}]').absent? >> any).repeat).as(:value)).maybe
   }
 
   rule (:enum_list) {
