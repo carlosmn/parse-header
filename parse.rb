34afbd4de108ad84e2775c9c55d1c72b0fbe4de3
Carlos Martín Nieto <carlos@cmartin.tk>
2012-05-23 13:39:03 +0200

    Use cparser by default

--- parse.rb
+++ parse.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 
-require './parser'
+#require './parser'
+require './cparser'
 require 'pp'
 
 file = open('diff.h')
 contents = file.read()
-parser = Parser.new
+parser = CParser::Parser.new
 # Get rid of preprocessor directives
 contents.gsub! /(^#.*)/, ''
 # And transform GIT_EXTERN(type) -> type
@@ -20,5 +21,3 @@ rescue Parslet::ParseFailed => error
   puts error, parser.root.error_tree
 end
 pp parsed
-line, col = parsed[3][:doc_comment].line_and_column
-puts "line and stuff #{line},#{col}"
